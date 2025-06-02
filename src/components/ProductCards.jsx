e226ad97020bb6546fff35b47ae034ecdcad31ab
devprince101 <75951797+devprince101@users.noreply.github.com>
2025-06-02 09:16:16 +0100

    Update Cards
    All the product cards are updated accordingly.

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Pouch1 from "../images/pouch1.png";
-import Pouch from "../images/pouch.png";
 import { Link } from "react-router-dom";
 
 const products = [
@@ -9,14 +7,14 @@ const products = [
     title: "Amazing Family Porridge",
     description:
       "A nourishing blend of maize, wheat, sorghum, soya, sesame, groundnuts, finger millet, and rice for growing children and families.",
-    image: require("../images/pouch1.png"), // Ensure the image is in the correct path
+    image: require("../images/pouch.jpg"), // Ensure the image is in the correct path
   },
   {
     id: 2,
     title: "Amazing Special Porridge",
     description:
       "A refined blend of sesame and soya, providing targeted wellness benefits for busy adults, especially women.",
-    image: require("../images/pouch.png"), // Ensure the image path is correct
+    image: require("../images/pouch.jpg"), // Ensure the image path is correct
   },
 ];
 
